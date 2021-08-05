cddeec31b9e8e60a404514863aca994272d48d8d
Jason Lu <jasonlu2907@gmail.com>
2021-08-04 22:22:07 -0500

    Setting up the project, navbar component, about component, search component, routing setting, bootstrap + bootswatch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    NavbarComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
